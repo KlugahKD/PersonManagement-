5beb52896ca9623a8181531c32642438e220262a
Klugah <Klugahdaniel1@gmail.com>
2023-08-31 17:41:22 +0000

    Created new controllers, models and views. And tried passing data with the ViewBag,ViewData and TempData.

---
+++ KkdManagementS/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using KkdManagementS.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KkdManagementS.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.greeting = "Hello klugah";
+            ViewData["greeting2"] = "Hello I am a male!";
+            //viewdata and viewbag can send data only from ControllerToView
+
+
+
+            //Tempdata can send message from one controller method to another controller method.
+            //it can also be used like the viewbag and viewdata.
+            TempData["greeting3"] = "I am 22 years of age";
+            return View();
+        }
+
+        public IActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            return View();
+        }
+    }
+}
---
+++ KkdManagementS/Models/Domain/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace KkdManagementS.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+
+
+    }
+}
